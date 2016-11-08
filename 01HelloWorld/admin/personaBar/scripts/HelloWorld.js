bf9b18eb7defbde9252141396833f65486ee86ce
Ash Prasad <ashishpd@hotmail.com>
2016-11-08 12:21:35 -0800

    Removed unnecessary JavaScript code

--- 01HelloWorld/admin/personaBar/scripts/HelloWorld.js
+++ 01HelloWorld/admin/personaBar/scripts/HelloWorld.js
@@ -1,39 +1,19 @@
 'use strict';
-define(['jquery',
-    '../scripts/config'
+define(['../scripts/config'
 ],
-    function ($, cf) {
-        var utility;
-        var config = cf.init();
+    function (cf) {
 
         return {
             init: function (wrapper, util, params, callback) {
-                utility = util;
-
-
-                if (typeof callback === "function") {
-                    callback();
-                }
             },
 
             initMobile: function (wrapper, util, params, callback) {
-                this.init(wrapper, util, params, callback);
             },
 
             load: function (params, callback) {
-                var fb = window.dnn.formBuilder;
-                if (fb && fb.load) {
-                    fb.load();
-                }
-                var optin = window.dnn.optIn;
-                if (optin && optin.load) {
-                    var mode = getOptInMode();
-                    optin.load(mode);
-                }
             },
 
             loadMobile: function (params, callback) {
-                this.load(params, callback);
             }
         };
     });
